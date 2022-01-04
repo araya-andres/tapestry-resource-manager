0fc8561e97113078a6968b8726438a19ecb05e09
Andres Araya <andresaraya@microsoft.com>
2022-01-04 12:57:16 -0600

    apply prettier

--- src/ResourceManager.js
+++ src/ResourceManager.js
@@ -1,79 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-const MIN = 0
-const MAX = 8
+const MIN = 0;
+const MAX = 8;
 
 class ResourceManager extends Component {
-    constructor() {
-        super()
-        this.state = {
-            resources: {
-                'coin': 1,
-                'worker': 1,
-                'food': 1,
-                'culture': 1,
-            },
-        }
+  constructor() {
+    super();
+    this.state = {
+      resources: {
+        coin: 1,
+        worker: 1,
+        food: 1,
+        culture: 1,
+      },
+    };
+  }
+
+  Increase = (resourceName) => () => {
+    const resources = this.state.resources;
+    const amount = resources[resourceName];
+    if (amount < MAX) {
+      this.setState({
+        resources: {
+          ...resources,
+          [resourceName]: amount + 1,
+        },
+      });
     }
+  };
 
-    Increase = resourceName => () => {
-        const resources = this.state.resources
-        const amount = resources[resourceName]
-        if (amount < MAX) {
-            this.setState({
-                resources: {
-                    ...resources,
-                    [resourceName]: amount + 1,
-                },
-            })
-        }
+  Decrease = (resourceName) => () => {
+    const resources = this.state.resources;
+    const amount = resources[resourceName];
+    if (amount > MIN) {
+      this.setState({
+        resources: {
+          ...resources,
+          [resourceName]: amount - 1,
+        },
+      });
     }
+  };
 
-    Decrease = resourceName => () => {
-        const resources = this.state.resources
-        const amount = resources[resourceName]
-        if (amount > MIN) {
-            this.setState({
-                resources: {
-                    ...resources,
-                    [resourceName]: amount - 1,
-                },
-            })
-        }
-    }
+  MakePlusMinusControl = (resourceName) => (
+    <div key={resourceName} className="PlusMinusBox">
+      <div className="ResourcePicture">
+        <img src={`./images/${resourceName}.png`} height="30" />
+      </div>
+      <div className="ResourceValue">
+        <h2>{this.state.resources[resourceName]}</h2>
+      </div>
+      <button className="PlusMinusButton" onClick={this.Increase(resourceName)}>
+        +
+      </button>
+      <button className="PlusMinusButton" onClick={this.Decrease(resourceName)}>
+        -
+      </button>
+    </div>
+  );
 
-    MakePlusMinusControl = resourceName => (
-        <div key={resourceName} className='PlusMinusBox'>
-            <div className='ResourcePicture'>
-                <img src={`./images/${resourceName}.png`} height="30"/>
-            </div>
-            <div className='ResourceValue'>
-                <h2>
-                    {this.state.resources[resourceName]}
-                </h2>
-            </div>
-            <button className='PlusMinusButton'
-                    onClick={this.Increase(resourceName)}
-            >
-                +
-            </button>
-            <button className='PlusMinusButton'
-                    onClick={this.Decrease(resourceName)}
-            >
-                -
-            </button>
-        </div>
-    )
-
-    render = () => (
-        <div>
-            {
-                Object.keys(this.state.resources).map(resource =>
-                    this.MakePlusMinusControl(resource)
-                )
-            }
-        </div>
-    )
+  render = () => (
+    <div>
+      {Object.keys(this.state.resources).map((resource) =>
+        this.MakePlusMinusControl(resource)
+      )}
+    </div>
+  );
 }
 
-export default ResourceManager
+export default ResourceManager;
